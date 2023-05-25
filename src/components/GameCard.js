8bf6b3ffd372fe141fa172256af4bd421d5ad925
Ryan McGinty <mcginty.ryan127@gmail.com>
2023-05-24 22:56:01 -0700

    asdasd

---
+++ src/components/GameCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import './gamecard.css'
+import { faThumbsUp, faThumbsDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+const GameCard = ({ gameCover, gameTitle, handleDislikeClick, handleLikeClick }) => {
+
+    const handleLike = () => {
+        handleLikeClick();
+    };
+
+    const handleDislike = () => {
+        handleDislikeClick();
+    };
+
+    return (
+        <div className="game-card">
+            <div className="game-image">
+                <img src={gameCover} alt="game" style={{ maxWidth: '80vw', maxHeight: '80vh' }} />
+            </div>
+            <h3 className="fixed-title">{gameTitle}</h3>
+            <div className='like-dislike-buttons'>
+                <button className='dislike-button' onClick={handleDislike}>
+                    <FontAwesomeIcon icon={faThumbsDown}>LT</FontAwesomeIcon>
+                </button>
+                <button className='like-button' onClick={handleLike}>
+                    <FontAwesomeIcon icon={faThumbsUp}>RT</FontAwesomeIcon>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default GameCard;
